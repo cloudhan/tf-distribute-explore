934d978d120b0c13751189cedf50d0200a1142ec
Cloud Han <cloudhan@outlook.com>
2020-10-13 14:47:01 +0800

    more about GradientTape

--- 3_new_op/show_custom_gradient.py
+++ 3_new_op/show_custom_gradient.py
@@ -15,8 +15,11 @@ def my_relu(x):
 
 
 if __name__ == "__main__":
-    x = tf.Variable(tf.constant([-1.0, 0.0, 1.0, 2.0, 3.0]))
+    x = tf.constant([-1.0, 0.0, 1.0, 2.0, 3.0])
     with tf.GradientTape() as tape:
+        # NOTE: by default, tape only watch tf.Variable, to compute gradient w.r.t. tensor, watch it.
+        tape.watch(x)
+
         y = my_relu(x)
 
     print(y)
